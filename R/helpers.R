caaa39afda1f2e6dbbac7c90bb896644b434745d
Miren BZ <mail@miren.bz>
2024-09-06 20:40:10 +0200

    Begin translation of EDA

--- R/helpers.R
+++ R/helpers.R
@@ -2,13 +2,13 @@ create_buttons <- function(next_topic = "#") {
   if (is.null(next_topic)) {
     next_button <- ""
   } else {
-    next_button <- glue::glue('<a id="nextTopicButton" class="btn btn-danger disabled" href="{next_topic}" role="button">Next topic</a>')
+    next_button <- glue::glue('<a id="nextTopicButton" class="btn btn-danger disabled" href="{next_topic}" role="button">Siguiente tema</a>')
   }
   button_section <- glue::glue('
 <section id="buttons">
-<button id="continueButton" class="btn btn-primary float-start me-2">Continue</button>
+<button id="continueButton" class="btn btn-primary float-start me-2">Continuar</button>
 {next_button}
-<button id="resetButton" class="btn btn-outline-warning btn-sm float-end">Start over</button>
+<button id="resetButton" class="btn btn-outline-warning btn-sm float-end">Volver a empezar</button>
 </section>')
   
   cat(button_section)
